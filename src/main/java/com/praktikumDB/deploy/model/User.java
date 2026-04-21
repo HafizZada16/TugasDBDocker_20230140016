c9f752bd9b5f917041fb1dde95a9e87bdfcb1a37
HafizZada16 <hafizzada15@gmail.com>
2026-04-21 14:53:37 +0700

    Fix error2

--- src/main/java/com/praktikumDB/deploy/model/User.java
+++ src/main/java/com/praktikumDB/deploy/model/User.java
@@ -12,4 +12,28 @@ public class User {
     private String id;
     private String name;
     private String nim;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
 }
