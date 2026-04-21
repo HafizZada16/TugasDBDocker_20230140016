fbb6a1ea756b76f0c9c3e93e82399f8868d072fd
HafizZada16 <hafizzada15@gmail.com>
2026-04-21 14:09:51 +0700

    Add User.java

---
+++ src/main/java/com/praktikumDB/deploy/model/User.java
@@ -0,0 +1,11 @@
+package com.praktikumDB.deploy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table (name = "20230140016")
+public class User {
+}
